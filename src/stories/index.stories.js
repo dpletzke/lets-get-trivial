fd1477694a4bd1bc790831e6efca183793f79f7c
skendanavian <s.nissen.bass@gmail.com>
2020-11-20 09:45:11 -0600

    edited stories

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -3,7 +3,6 @@ import Loader from "react-loader-spinner";
 
 import Button from "../components/Button";
 import Loading from "../components/Loading";
-import Title from "../components/Title";
 
 storiesOf("Button", module).add("Submit", () => <Button>Submit</Button>);
 storiesOf("Loading", module).add("Dots", () => (
