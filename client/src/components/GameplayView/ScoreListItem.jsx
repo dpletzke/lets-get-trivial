7c99a9e8da7ca91c7ee0605bf3dd744560f63e7c
Sebastian Kann <sebastiankann@gmail.com>
2020-11-27 13:24:07 -0500

    styled scoredboard

--- client/src/components/GameplayView/ScoreListItem.jsx
+++ client/src/components/GameplayView/ScoreListItem.jsx
@@ -1,21 +1,30 @@
 import "./ScoreListItem.scss";
-import { FaUser } from "react-icons/fa";
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
+import classNames from 'classnames';
+
 
 function ScoreListItem({ name, score, position, pointsEarned }) {
+
+  const upArrow = classNames("upArrow", {"upArrow--show": (pointsEarned > 0)});
+  const downArrow = classNames("downArrow", {"downArrow--show": (pointsEarned < 0)});
+  const playerHeading = classNames("player-heading", {"player-heading--firstPlace" : (position===1)})
+
   return (
     <div className="score-item">
-      <div className="player-heading">
+      <div className={playerHeading}>
+        <p className="position"> {position} </p>
         <p>
           {/* <FaUser className="player-icon" /> */}
-         {position} {name}
+        {name}
         </p>
         <div className="info-group">
-          <p>
-            Score:
-          </p>
+          
             <p className="dark">{score}</p>
+            <div className="points-group">
             <p className="dark">{pointsEarned}</p>
-       
+            <p className={upArrow}><FaArrowUp/></p>
+            <p className={downArrow}><FaArrowDown/></p>
+            </div>
         </div>
       </div>
     </div>
