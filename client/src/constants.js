42fd2f01b29192726f3581a0885862eb2418d5c8
skendanavian <s.nissen.bass@gmail.com>
2020-11-28 15:12:03 -0600

    set constants back to 2s

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
 export const LAG_BEFORE_SEND_ANSWER = 2000;
-export const SCOREBOARD_LAG = 8000;
-export const STARTPAGE_LAG = 8000;
+export const SCOREBOARD_LAG = 2000;
+export const STARTPAGE_LAG = 2000;
