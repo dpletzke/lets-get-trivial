fa95b87830063f8493fdc5deadfdd75208f5e940
daniel <dpletzke@gmail.com>
2020-11-30 14:12:46 -0600

    copy icon and message working

--- client/src/components/WaitingRoom/PlayerListItem.jsx
+++ client/src/components/WaitingRoom/PlayerListItem.jsx
@@ -1,14 +1,34 @@
+import {useState} from 'react';
 import "./PlayerListItem.scss";
 import { FaUser } from "react-icons/fa";
 
+import { IoIosCopy } from "react-icons/io";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+const classNames = require('classnames');
+
 function PlayerListItem(props) {
+  const [copied, setCopied] = useState(false)
   const { name, gameIdItem, playerItem } = props;
+
+  const copyClass = classNames("copy-message", {"copy-message--show": copied});
+
+  const setCopy = () => {
+    if (copied === false){
+      setCopied(true)
+      setTimeout(() => {setCopied(false)}, 2000)
+    }
+  }
 
   return (
     <div className="player-container">
-      {gameIdItem && <p className="game-id">Game ID:</p>}
       {gameIdItem && (
-        <h2 className="list-item-text game-id id-text"> {name}</h2>
+        <CopyToClipboard text={name}>
+          <div className="clipboard-content" onClick={() => setCopy(true)} >
+            <p className="game-id">Game ID:</p>
+            <h2 className="list-item-text game-id id-text"> {name}            <IoIosCopy   className="icon clipboard-icon" /><p className={copyClass} style={{display:'hidden', fontSize:'0.1rem' }}>Copied!</p></h2>
+          </div>
+        </CopyToClipboard>
       )}
       {playerItem && (
         <p className="list-item-text">
--- client/src/components/WaitingRoom/index.jsx
+++ client/src/components/WaitingRoom/index.jsx
@@ -10,6 +10,7 @@ import GameRules from "../GameRules";
 import ModalComponent from "../Modal";
 import { FaCog, FaQuestion, FaVolumeMute, FaVolumeUp } from "react-icons/fa";
 import { ImExit } from "react-icons/im";
+import { IoIosCopy } from "react-icons/io";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import ConnectionContext from "../../ConnectionContext";
@@ -70,9 +71,6 @@ function WaitingRoom(props) {
             </header>
             <h1 className="box-waiting--header">Waiting for Players...</h1>
             <PlayerListItem className="alt-text" name={gameId} gameIdItem />
-            <CopyToClipboard text={gameId}>
-              <button>stuff</button>
-            </CopyToClipboard>
             <Button className="pulse" onClick={startGame} gameRoom>
               Start Game >>
             </Button>
